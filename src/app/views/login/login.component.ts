c039e39e9b63491c7a6e8a198f924834c464c86d
LucaLuz <luiza24021@gmal.com>
2022-04-09 18:09:35 -0300

    commit 3

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -15,21 +15,53 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     // this.onSubmit()
   }
-  loginModel = new User("","",);
   // userModel = new User ("","")
+  loginModel = new User("","",);
   mensagem = ""
-  
+  usuarioLogado = ""
+
   logar() {
     console.log(this.loginModel)
 
-    this.loginService.login(this.loginModel).subscribe((Response)=>{
-     this.router.navigateByUrl('')
-    }, (respostaErro)=>{
-      this.mensagem = respostaErro.error
-      console.log(this.mensagem)
+    
+    let erroEncontrado = 0;
+
+    const listaPalavras: string[] = ["select ", "from ", "drop ", "or ", "having ", "group ", "insert ", "exec ", "\"", "\'", "--", "#", "*", ";"]
+
+    listaPalavras.forEach(palavra => {
+      console.log("palavra atual:", palavra)
+      
+      if(this.loginModel.email.toLowerCase().includes(palavra)) {
+        console.log("Palavra encontrada:", palavra)
+        this.mensagem = "Dados inválidos: " + palavra;
+
+        erroEncontrado = 1;
+      }
+
     })
+
+    if (erroEncontrado == 0) {
+      this.loginService.login(this.loginModel).subscribe( (response) => {
+        this.usuarioLogado = response.body.user.nome
+        // console.log(response)
+        console.log(this.usuarioLogado)
+        // this.router.navigateByUrl('')
+      }, (respostaErro) => {
+        this.mensagem = respostaErro.error
+      })      
+    }
   }
+
 }
+
+//     this.loginService.login(this.loginModel).subscribe((Response)=>{
+//      this.router.navigateByUrl('')
+//     }, (respostaErro)=>{
+//       this.mensagem = respostaErro.error
+//       console.log(this.mensagem)
+//     })
+//   }
+// }
   // onSubmit(){
   //   console.log(this.userModel)
   // }
